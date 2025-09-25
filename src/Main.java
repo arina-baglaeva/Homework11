b46184ad772a77cce6e6af16f381897ca873ccf5
ariba <ariba2576@gmail.com>
2025-09-25 20:35:25 +0300

    HM-11 done

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,58 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int year = 2000;
+        task1VisocosniOrNot(year);
+        byte clientOs = 1;
+        int currentYear = LocalDate.now().getYear();
+        int rastToClient = 95;
+        task2VersionApp(currentYear, clientOs);
+        task3Dostavka(rastToClient);
+    }
+    public static void task1VisocosniOrNot(int year)
+    {
+        if(year%4==0 && year%100!=0){
+            System.out.println(year +" год является високосным.");
+        }else if (year%400==0){
+            System.out.println(year +" год является високосным.");
+        }else{
+            System.out.println(year +" год не является високосным.");
         }
+
+    }
+    public static void task2VersionApp( int clientDeviceYear, byte clientOS)
+    {
+        System.out.println("Текущий год: "+ clientDeviceYear);
+        if(clientDeviceYear <2015){
+            if (clientOS == 0){
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+            }else{
+                System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+            }
+        }else{
+            if (clientOS == 0){
+                System.out.println("Установите версию приложения для iOS по ссылке.");
+            }else{
+                System.out.println("Установите версию приложения для Android по ссылке.");
+            }
+        }
+
+    }
+    public static void task3Dostavka(int distance)
+    {
+        System.out.println("Расстояние до клиента: "+ distance);
+        if (distance <=20){
+            System.out.println("Потребуется дней: 1 ");
+        }
+        else if(20< distance && distance<60){
+
+            System.out.println("Потребуется дней: 2 ");
+        }else if (60<= distance && distance<=100){
+
+            System.out.println("Потребуется дней: 3 ");
+        }else{
+            System.out.println("Нет доставки.");
+        }
+
     }
 }
